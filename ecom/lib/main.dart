4309f5918abbbdd16eef3254a5dcca6472e0c9f5
Qanber Ali <qamberali418@gmail.com>
2021-06-28 23:08:46 +0500

    Ecomm ui

---
+++ ecom/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'cookie_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+
+        primarySwatch: Colors.blue,
+      ),
+      home: (Scaffold(
+        body: CookiePage(),
+      )),
+    );
+  }
+}
